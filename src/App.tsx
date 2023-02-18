24aa1498cf3fc5cee5338369ae24412a5afbe1d1
Sameh <sameh.abdelhaleem95@gmail.com>
2023-02-19 00:39:17 +0200

    add newTodo feature using state and TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
+import { useState } from "react";
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  // const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const addTodoHandler = (text: string) => {
+    const newTodo = new Todo(text);
+    setTodos((prevState) => {
+      return prevState.concat(newTodo);
+    });
+    console.log(text);
+  };
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
-      <NewTodo />
     </div>
   );
 }
